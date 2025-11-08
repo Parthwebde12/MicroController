196252d6f378554415806b82762b74fea7f16891
Parth Wakodikar <163313000+Parthwebde12@users.noreply.github.com>
2025-11-08 18:58:16 +0530

    Update BCD.c

---
+++ BCD.c
@@ -0,0 +1,24 @@
+#include <reg51.h>
+#include <stdio.h>
+
+unsigned char led_status = 0x00;
+
+void delay() {
+    int i, j;
+    for(i = 0; i < 1000; i++) {
+        for(j = 0; j < 1000; j++) {
+        }
+    }
+}
+
+void main() {
+    P1 = led_status;
+    while(1) {
+        P1 = led_status;
+        if(led_status == 0xFF)
+            led_status = 0x00;
+        else
+            led_status = led_status + 1;
+        delay();
+    }
+}
