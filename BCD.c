b515286882536d0c89a7a1b36d42c62724d7c29f
Parth Wakodikar <163313000+Parthwebde12@users.noreply.github.com>
2025-11-09 07:44:03 +0530

    Update BCD.c

--- BCD.c
+++ BCD.c
@@ -7,18 +7,22 @@ void delay() {
     int i, j;
     for(i = 0; i < 1000; i++) {
         for(j = 0; j < 1000; j++) {
+            // Software delay
         }
     }
 }
 
 void main() {
-    P1 = led_status;
+    P1 = led_status;  // Initialize LEDs
+
     while(1) {
-        P1 = led_status;
+        P1 = led_status;   // Output LED pattern
+
         if(led_status == 0xFF)
             led_status = 0x00;
         else
             led_status = led_status + 1;
-        delay();
+
+        delay();  // Delay between increments
     }
 }
